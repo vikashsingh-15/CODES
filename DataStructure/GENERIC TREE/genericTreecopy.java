522fdf07f5731fec281d4fa903051a675888261d
vikashsingh-15 <writetovikashsingh@gmail.com>
2022-01-15 12:23:12 +0530

    commit

--- DataStructure/GENERIC TREE/genericTreecopy.java
+++ DataStructure/GENERIC TREE/genericTreecopy.java
@@ -5,7 +5,7 @@ import java.util.*;
 // 24
 // 10 20 50 -1 60 -1 -1 30 70 -1 80 110 -1 120 -1 -1 90 -1 -1 40 100 -1 -1 -1 
 
-public class genericTree1
+public class genericTreecopy
 {
   public static class Node
    {
